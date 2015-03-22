f1b6a1b8deb6808fcbdf3d2d9feff7bd86413a0f
Camilo Roca <carocad@unal.edu.co>
2015-03-22 18:52:34 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,30 @@
+"""
+daVinci generates html files with drawings of your Python project source code.
+
+Usage:
+	daVinci draw [<PATH>]
+	daVinci -h | --help
+	daVinci --version
+
+Options:
+	-h --help Show this screen.
+	--version Show version.
+
+"""
+
+from os import getcwdu
+from docopt import docopt
+from pyclbr import readmodule_ex
+
+
+def _main():
+	'''daVinci generates html files with drawings of your Python project source code.'''
+  	arguments = docopt(__doc__, version = '0.0.1')
+
+  	if arguments['<PATH>']:
+		print('test')
+	else:
+		print(__doc__)
+
+if __name__ == '__main__':
+	_main()
