4a86928405f8cff706433574682d7d40174a3c40
Camilo Roca <carocad@unal.edu.co>
2015-03-23 00:34:52 +0100

    search and printing of Classes and function definitions.

--- main.py
+++ main.py
@@ -12,9 +12,10 @@ Options:
 
 """
 
-from os import getcwdu
+from os import getcwdu, walk, path
 from docopt import docopt
-from pyclbr import readmodule_ex
+from pyclbr import readmodule_ex, Class, Function
+from operator import itemgetter
 
 
 def _main():
@@ -22,7 +23,33 @@ def _main():
   	arguments = docopt(__doc__, version = '0.0.1')
 
   	if arguments['draw']:
-		print('test')
+		modules = {}
+		cwd = getcwdu()
+		for path, dirs, files in walk(cwd):
+			for file in files:
+				if file.endswith('.py'):
+					modules[file[:-3]] = path
+
+		definitions = []
+		for name, path in modules.items():
+			try:
+				dict = readmodule_ex(name, [path])
+			except ImportError:
+				print "Error: failed to find module: ", name, " in ", path
+			classes = dict.values()
+			if classes:
+				definitions.extend(classes)
+
+		for obj in definitions:
+			if isinstance(obj, Class):
+				print "class", obj.name, obj.super, obj.lineno
+				methods = sorted(obj.methods.iteritems(), key=itemgetter(1))
+				for name, lineno in methods:
+					if name != "__path__":
+						print "  def", name, lineno
+			elif isinstance(obj, Function):
+				print "def", obj.name, obj.lineno
+
 	else:
 		print(__doc__)
 
