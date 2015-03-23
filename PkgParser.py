17169e5f56ddcf86f3a4219878a0b1cda2cdfb3a
Camilo Roca <carocad@unal.edu.co>
2015-03-23 18:40:48 +0100

    PkgParser class for search and manipulation of the class browser

---
+++ PkgParser.py
@@ -0,0 +1,35 @@
+from os import getcwd, walk
+from PyFunction import PyFunction
+from PyClass import PyClass
+from pyclbr import readmodule_ex, Class, Function
+
+class PkgParser():
+
+	def __init__(self, path):
+		self.path = path
+		self.defs = {}
+
+	def parse(self):
+		modules = {}
+		for path, dirs, files in walk(self.path):
+			for filename in files:
+				if filename.endswith('.py'):
+					modules[filename[:-3]] = path
+
+		for name, path in modules.items():
+			try:
+				tmp_defs = readmodule_ex(name, [path])
+				for name, obj in tmp_defs.items():
+					if isinstance(obj, Class):
+						self.defs[name] = PyClass(obj)
+					elif isinstance(obj, Function):
+						self.defs[name] = PyFunction(obj)
+			except ImportError as error:
+				print(error)
+
+	def defsToString(self):
+		result = ""
+		for definition in self.defs.values():
+			result += repr(definition)
+
+		return result
