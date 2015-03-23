a750781494f60b60238763c30aeca1f1a3ef72bb
Camilo Roca <carocad@unal.edu.co>
2015-03-23 15:19:44 +0100

    code ported to python3

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ Options:
 
 """
 
-from os import getcwdu, walk, path
+from os import getcwd, walk
 from docopt import docopt
 from pyclbr import readmodule_ex, Class, Function
 from operator import itemgetter
@@ -20,35 +20,35 @@ from operator import itemgetter
 
 def _main():
 	'''daVinci generates html files with drawings of your Python project source code.'''
-  	arguments = docopt(__doc__, version = '0.0.1')
+	arguments = docopt(__doc__, version = '0.0.1')
 
-  	if arguments['draw']:
+	if arguments['draw']:
 		modules = {}
-		cwd = getcwdu()
+		cwd = getcwd()
 		for path, dirs, files in walk(cwd):
-			for file in files:
-				if file.endswith('.py'):
-					modules[file[:-3]] = path
+			for filename in files:
+				if filename.endswith('.py'):
+					modules[filename[:-3]] = path
 
 		definitions = []
-		for name, path in modules.items():
+		for name, path in list(modules.items()):
 			try:
 				dict = readmodule_ex(name, [path])
 			except ImportError:
-				print "Error: failed to find module: ", name, " in ", path
-			classes = dict.values()
+				print("Error: failed to find module: ", name, " in ", path)
+			classes = list(dict.values())
 			if classes:
 				definitions.extend(classes)
 
 		for obj in definitions:
 			if isinstance(obj, Class):
-				print "class", obj.name, obj.super, obj.lineno
-				methods = sorted(obj.methods.iteritems(), key=itemgetter(1))
+				print("class", obj.name, obj.super, obj.lineno)
+				methods = sorted(iter(obj.methods.items()), key=itemgetter(1))
 				for name, lineno in methods:
 					if name != "__path__":
-						print "  def", name, lineno
+						print("  def", name, lineno)
 			elif isinstance(obj, Function):
-				print "def", obj.name, obj.lineno
+				print("def", obj.name, obj.lineno)
 
 	else:
 		print(__doc__)
