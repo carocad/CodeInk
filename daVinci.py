0bb5594e6f06df87b61440a88023191af3d450bd
Camilo Roca <carocad@unal.edu.co>
2015-04-17 16:38:25 +0200

    parse_package() refactored. fetch_imports() and merge_imports() functions created.

--- daVinci.py
+++ daVinci.py
@@ -1,11 +1,16 @@
 """Module to find the Python files in a directory and parse them"""
 
 import os
-import pyPeephole
 import collections
+import networkx
+import pyPeephole
 
 def draw(path):
+	mind = networkx.Graph()
 	modules = parse_package(path)
+	imports = fetch_imports(modules)
+	merge_imports(imports)
+
 
 def search(path, name):
 	""" find the function or class definition with the specidfied name """
@@ -19,20 +24,37 @@ def search(path, name):
 		print("\n I couldn't found the definition called {name}.\n".format(name=name),
 			  "Check if the name is correct")
 
+def merge_imports(import_tuples):
+	IMPORT_G = collections.namedtuple('import_group', 'name used_in')
+	cache = {}
+	for single in import_tuples:
+		if single.name in cache:
+			cache[single.name].used_in.append(single.path + ":L" + str(single.lineno))
+		else:
+			cache[single.name] = IMPORT_G._make([single.name,
+												[single.path + ":L" + str(single.lineno)]])
+
+def fetch_imports(modules):
+	IMPORT_S = collections.namedtuple('import_single', 'name path lineno')
+	for module in modules:
+		for node in pyPeephole.get_imports(module.tree):
+			for name in node.names:
+				yield IMPORT_S._make([name.name, module.path, node.lineno])
+		for node in pyPeephole.get_importsFrom(module.tree):
+			yield IMPORT_S._make([node.module, module.path, node.lineno])
+
+def parse_package(path):
+	""" parse a Python package recursively and extract functions, classes
+	 	and imports defined there"""
+	MODULE = collections.namedtuple('module', 'name path tree')
+	for path in find_pyFiles(path):
+		name = os.path.basename(path)[:-3] #take the .py away
+		tree = pyPeephole.parse(path)
+		yield MODULE._make([name, path, tree])
+
 def find_pyFiles(path):
 	"find Python source code files contained in a directory"
 	for path, dirs, files in os.walk(path):
 		for filename in files:
 			if filename.endswith('.py'):
 				yield os.path.join(path, filename)
-
-def parse_package(path):
-	""" parse all Python source code files contained in a directory and extract
-		their functions and classes declarations"""
-	MODULE = collections.namedtuple('module', 'name path tree')
-	modules = set()
-	for path in find_pyFiles(path):
-		tree = pyPeephole.parse(path)
-		name = os.path.basename(path)[:-3] #take the .py away
-		modules.add(MODULE._make([name, path, tree]))
-	return modules
