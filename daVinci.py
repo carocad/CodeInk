18b269b9b5d5c5bef2462d860a028faa7d76af8b
Camilo Roca <carocad@unal.edu.co>
2015-04-19 19:36:52 +0200

    daVinci module almost totally rewritten to use: pkgutil, networkx.Graph methods, new version of pyPeephole

--- daVinci.py
+++ daVinci.py
@@ -4,13 +4,52 @@ import os
 import collections
 import networkx
 import pyPeephole
+import pkgutil
 
 def draw(path):
 	mind = networkx.Graph()
-	modules = parse_package(path)
-	imports = fetch_imports(modules)
-	merge_imports(imports)
+	#TODO create a graph base on the imports, functions and classes
+	pkg_dirs = {path for path, dirs, files in os.walk(path)} # set comprehension
+	pkg_modules = {name:path.path for path, name, ispkg in pkgutil.iter_modules(pkg_dirs)
+				   					if hasattr(path, 'path')
+				   					and not ispkg} #dict comprehension
+	builtin_modules = {name:path.path for path, name, ispkg in pkgutil.iter_modules()
+					  					if hasattr(path, 'path')
+					   					and path.path not in pkg_dirs} #dict comprehension
 
+	#MODULE = collections.namedtuple('modules', 'name path doc definitions')
+	#IMPORT = collections.namedtuple('imports', 'module path lineno definition')
+	#CLASS = collections.namedtuple('classes', 'module name path lineno doc definition')
+	#FUNCTION = collections.namedtuple('functions', 'module name path lineno doc definition')
+	for module_name, directory in pkg_modules.items():
+		filepath = os.path.join(directory, module_name + '.py')
+		tree = pyPeephole.parse(filepath)
+		if not tree: # in case of invalid syntax
+			print('module avoided')
+			continue
+		mod_doc = pyPeephole.get_attr(tree, 'doc')
+		mind.add_node(id(module_name), type='module', name=module_name,
+					  doc=mod_doc, path=filepath)
+		for node in pyPeephole.get_functions(tree):
+			func_doc = pyPeephole.get_attr(node, 'doc')
+			mind.add_node(id(node.name), type='function', name=node.name,
+						  doc=func_doc, definition=pyPeephole.to_string(node),
+						  path=filepath, lineno=node.lineno)
+			mind.add_edge(id(module_name), id(node.name))
+		for node in pyPeephole.get_classes(tree):
+			class_doc = pyPeephole.get_attr(node, 'doc')
+			mind.add_node(id(node.name), type='class', name=node.name,
+						  doc=class_doc, definition=pyPeephole.to_string(node),
+						  path=filepath, lineno=node.lineno)
+			mind.add_edge(id(module_name), id(node.name))
+			#TODO add the class methods to the graph
+
+#			elif isinstance(node, ast.ClassDef):
+#			elif isinstance(node, ast.Import):
+#			elif isinstance(node, ast.ImportFrom):
+	for node_id in mind.nodes_iter():
+		print(mind.node[node_id], '\n')
+	print(mind.edges())
 
 def search(path, name):
 	""" find the function or class definition with the specidfied name """
@@ -23,38 +62,3 @@ def search(path, name):
 	else:
 		print("\n I couldn't found the definition called {name}.\n".format(name=name),
 			  "Check if the name is correct")
-
-def merge_imports(import_tuples):
-	IMPORT_G = collections.namedtuple('import_group', 'name used_in')
-	cache = {}
-	for single in import_tuples:
-		if single.name in cache:
-			cache[single.name].used_in.append(single.path + ":L" + str(single.lineno))
-		else:
-			cache[single.name] = IMPORT_G._make([single.name,
-												[single.path + ":L" + str(single.lineno)]])
-
-def fetch_imports(modules):
-	IMPORT_S = collections.namedtuple('import_single', 'name path lineno')
-	for module in modules:
-		for node in pyPeephole.get_imports(module.tree):
-			for name in node.names:
-				yield IMPORT_S._make([name.name, module.path, node.lineno])
-		for node in pyPeephole.get_importsFrom(module.tree):
-			yield IMPORT_S._make([node.module, module.path, node.lineno])
-
-def parse_package(path):
-	""" parse a Python package recursively and extract functions, classes
-	 	and imports defined there"""
-	MODULE = collections.namedtuple('module', 'name path tree')
-	for path in find_pyFiles(path):
-		name = os.path.basename(path)[:-3] #take the .py away
-		tree = pyPeephole.parse(path)
-		yield MODULE._make([name, path, tree])
-
-def find_pyFiles(path):
-	"find Python source code files contained in a directory"
-	for path, dirs, files in os.walk(path):
-		for filename in files:
-			if filename.endswith('.py'):
-				yield os.path.join(path, filename)
