d77515652b4d309685c3ce983eaf068850af1ba0
Camilo Roca <carocad@unal.edu.co>
2015-05-30 16:51:46 +0200

    tabs replaced by spaces

--- tests/test_atelier/test_scientist.py
+++ tests/test_atelier/test_scientist.py
@@ -6,29 +6,28 @@ from codeink.atelier import scientist
 from codeink.parchment import pkginfo
 
 def test_check_complexity():
-	parent_dir = os.path.dirname(os.path.dirname(__file__))
-	guineapig_path = os.path.join(parent_dir, 'guinea-pig',
-								  'cage2', 'pig2.py')
-	default = 80
-	#empty file --> complexity = 1
-	answer = (default+1, 'hsl(120.0, 90%, 40%)')
-	assert answer == scientist.check_complexity(guineapig_path)
+    parent_dir = os.path.dirname(os.path.dirname(__file__))
+    guineapig_path = os.path.join(parent_dir, 'guinea-pig', 'cage2', 'pig2.py')
+    default = 80
+
+    answer = (default+1, 'hsl(120.0, 90%, 40%)')
+    assert answer == scientist.check_complexity(guineapig_path)
 
 def test_compute_edges():
-	parent_dir = os.path.dirname(os.path.dirname(__file__))
-	guineapig_path = os.path.join(parent_dir, 'guinea-pig')
-	master_module = os.path.join(parent_dir, 'guinea-pig', 'lab_assistant.py')
-	pig1_path = os.path.join(parent_dir, 'guinea-pig', 'cage1', 'pig1.py')
+    parent_dir = os.path.dirname(os.path.dirname(__file__))
+    guineapig_path = os.path.join(parent_dir, 'guinea-pig')
+    master_module = os.path.join(parent_dir, 'guinea-pig', 'lab_assistant.py')
+    pig1_path = os.path.join(parent_dir, 'guinea-pig', 'cage1', 'pig1.py')
+    pig2_path = os.path.join(parent_dir, 'guinea-pig', 'cage2', 'pig2.py')
 
-	dirs = list(pkginfo.get_directories(guineapig_path, '.py'))
-	finder = modulefinder.ModuleFinder(path=dirs)
-	finder.run_script(master_module)
-	base = 'mammal'
-	edges = set(scientist.compute_edges(master_module, base, finder.modules.values(),
-										 finder.badmodules.keys()))
-	for edge in edges:
-		print(edge)
-	answer = set([(master_module, base),
-				  (master_module, pig1_path)])
-	assert answer == edges
+    dirs = list(pkginfo.get_directories(guineapig_path, '.py'))
+    finder = modulefinder.ModuleFinder(path=dirs)
+    finder.run_script(master_module)
+    base = 'mammal'
+    edges = set(scientist.compute_edges(master_module, base, finder.modules.values(),
+                                        finder.badmodules.keys()))
+    answer = set([(master_module, base),
+                  (master_module, pig1_path),
+                  (master_module, pig2_path)])
+    assert answer == edges
 
