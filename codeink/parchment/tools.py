33a77233f28d0015244a70e4df58b9bc66622264
Camilo Roca <carocad@unal.edu.co>
2015-06-09 15:06:51 +0200

    safe_import docstring

--- codeink/parchment/tools.py
+++ codeink/parchment/tools.py
@@ -1,6 +1,15 @@
 
 def safe_import(origin, funk1, funk2):
-    """Safely import a function whose name was changed from a module whose name was not
+    """Safely import a function whose name was changed from a module whose name was not.
+    This function is specially useful if the function name is not known at runtime.
+    
+    Args:
+    	origin (str): name of the module where the function to be imported is located
+    	funk1 (str): name of the first function name to try to import
+    	funk2 (str): name of the second function name to try to import
+    Returns:
+    	function: function object imported from `origin`
+    	
     Example:
     # instead of writting this
         try:
