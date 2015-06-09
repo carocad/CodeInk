7e1ebe3f8510270512482032ea19fca9b20c5ddf
Camilo Roca <carocad@unal.edu.co>
2015-06-09 15:22:55 +0200

    fixed indentention problem

--- codeink/parchment/pkginfo.py
+++ codeink/parchment/pkginfo.py
@@ -7,7 +7,7 @@ import pkgutil
 import fnmatch
 
 def get_directories(dirpath, extension='.py'):
-	""" recursively get all subdirectories including `dirpath`
+    """ recursively get all subdirectories including `dirpath`
 	that contain files with the requested extension.
 
 	Args:
@@ -19,10 +19,10 @@ def get_directories(dirpath, extension='.py'):
 	        requested extension.
 	"""
 	for root, dirs, files in os.walk(dirpath):
-		for file in files:
-			if fnmatch.fnmatch(file, '*' + extension):
-				yield root
-				break
+	    for file in files:
+	        if fnmatch.fnmatch(file, '*' + extension):
+	            yield root
+	            break
 
 def is_package(absdirpath):
     """ checks if a directory is in fact a python package.
@@ -33,32 +33,32 @@ def is_package(absdirpath):
     Returns:
     	bool: True if the directory is a package, false otherwise.
     """
-	parent_dir = os.path.dirname(absdirpath)
-	pkg_name = os.path.basename(absdirpath)
-	for root, name, ispkg in pkgutil.iter_modules([parent_dir]):
-		if ispkg and name == pkg_name:
-			return True
-	return False
+    parent_dir = os.path.dirname(absdirpath)
+    pkg_name = os.path.basename(absdirpath)
+    for root, name, ispkg in pkgutil.iter_modules([parent_dir]):
+        if ispkg and name == pkg_name:
+            return True
+    return False
 
 def get_modules(directories):
-	""" get all the modules present in `directories`
-	
+    """ get all the modules present in `directories`
+
 	Args:
 	 	directories (list): list of directories where the modules
 	 		may lay in.
 	Yields:
-		str: absolute filepath of a module
+        str: absolute filepath of a module
 	"""
 	for root, name, ispkg in pkgutil.iter_modules(directories):
-		if hasattr(root, 'path') and not ispkg:
-			yield os.path.join(root.path, name + '.py')
+	    if hasattr(root, 'path') and not ispkg:
+	        yield os.path.join(root.path, name + '.py')
 
 def filter_modules(modpaths, patterns):
-	"""filter away modules that match a specific name pattern.
+    """filter away modules that match a specific name pattern.
 	Note: since modpaths are full filepaths, the name pattern
 	MUST be specified for the path itself, as opposed to only
 	the filename.
-	
+
 	Args:
 	 	modpaths (list): list of absolute modules filepath to
  			filter
@@ -69,14 +69,14 @@ def filter_modules(modpaths, patterns):
  			any pattern.
 	"""
 	for pattern in patterns:
-		modpaths = [ filepath for filepath in modpaths
-			  		 if not fnmatch.fnmatch(filepath, pattern) ]
+	    modpaths = [ filepath for filepath in modpaths
+	                 if not fnmatch.fnmatch(filepath, pattern) ]
 	return modpaths
 
 def find_root_pkg(absfilepath):
-	"""find the root package of a specific file, i.e. the most
+    """find the root package of a specific file, i.e. the most
 	upper directory that is considered a package.
-	
+
 	Args:
 	 	absfilepath (str): absolute filepath of the module whose 
 	 		root package is being searched.
@@ -86,6 +86,6 @@ def find_root_pkg(absfilepath):
 	rootpath = os.path.dirname(absfilepath)
 	ispkg = is_package(rootpath)
 	while ispkg:
-		rootpath = os.path.dirname(rootpath)
-		ispkg = is_package(rootpath)
+	    rootpath = os.path.dirname(rootpath)
+	    ispkg = is_package(rootpath)
 	return rootpath
